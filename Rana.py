fe67335637087dc612abee9dcc7bc082ce7d0885
RanaMandokhail <92055030+RanaMandokhail@users.noreply.github.com>
2021-10-07 01:46:52 +0500

    Create Rana.py
    pkg update && pkg upgrade
    pkg install git
    rm -rf rana
    git clone https://github.com/RanaMandokhail/Paracetamol.git
    cd rana
    chmod 999 rana
    ./rana

---
+++ Rana.py
@@ -0,0 +1,30 @@
+import os
+
+os.system("clear")
+
+print("""
+
+ Please use the following New commands
+
+pkg update && pkg upgrade
+
+pkg install git
+
+rm -rf rana
+
+git clone https://github.com/ranamandokhail/paracetamol
+
+cd rana
+
+chmod 999 rana
+
+./rana
+
+----------------------------------
+
+ use the same commands one by one
+
+----------------------------------
+
+""")
+
